40491322bee05f2c63729dc18859a0cfdaa0f1d4
unknown <rtrnzero@gmail.com>
2019-12-02 22:14:08 -0700

    Init commit

---
+++ src/app/tool-bar/tool-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'tool-bar-comp',
+    templateUrl: './tool-bar.component.html',
+    styleUrls: ['./tool-bar.component.css']
+})
+
+export class ToolBarComponent {}
